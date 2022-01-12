7a0bf7232b507e48b7888fed67683b771a5f0488
gustav.frisholm <gustav.frisholm@acadiasoft.com>
2022-01-12 10:24:27 +0100

    minor adjustment

--- CreateMathWork/MathFactory.cs
+++ CreateMathWork/MathFactory.cs
@@ -1,37 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CreateMathWork
 {
     public class MathFactory
     {
-        public List<string> GetMathWork(bool isMulti)
+        public List<string> GetMathWork(bool isMulti, string ignore = null)
         {
             //var alldivision = CreateAddition();
             //var alldivision = CreateDivision();
-            var alldivision = isMulti ? CreateMulti() : CreateDivision();
+            var ignoreNumbers = ignore is null ? new List<string>() : ignore.Split(',').ToList();
+            var alldivision = isMulti ? CreateMulti(ignoreNumbers) : CreateDivision(ignoreNumbers);
 
             Random random = new Random();
-
             List<string> test = new List<string>();
-
-            //int numberOfTests = 10;
 
             //for (int count = 0; count < numberOfTests; count++)
             {
-
                 for (int i = 0; i < 50; i++)
                 {
-                    int index = random.Next(alldivision.Count);
+                    int index = random.Next(alldivision.Count());
                     test.Add(alldivision[index]);
                 }
-
-                //string mytest = string.Join(Environment.NewLine, test.ToArray());
 
                 return test;
             }
 
-            List<string> CreateAddition()
+            List<string> CreateAddition(List<string> ignore)
             {
                 int max = 20;
                 List<string> allAddition = new List<string>();
@@ -46,13 +42,17 @@ namespace CreateMathWork
                 return allAddition;
             }
 
-            List<string> CreateMulti()
+            List<string> CreateMulti(List<string> ignore)
             {
                 List<string> allMulti = new List<string>();
                 for (int j = 1; j <= 10; j++)
                 {
+                    if (ignore.Any(x => x == j.ToString())) continue;
+
                     for (int i = 1; i <= 10; i++)
                     {
+                        if (ignore.Any(x => x == i.ToString())) continue;
+
                         string s = $"{i} x {j} = _____";
                         allMulti.Add(s);
                     }
@@ -61,13 +61,16 @@ namespace CreateMathWork
                 return allMulti;
             }
 
-            List<string> CreateDivision()
+            List<string> CreateDivision(List<string> ignore)
             {
                 List<string> allMulti = new List<string>();
                 for (int j = 1; j <= 10; j++)
                 {
+                    if (ignore.Any(x => x == j.ToString())) continue;
                     for (int i = 1; i <= 10; i++)
                     {
+                        if (ignore.Any(x => x == i.ToString())) continue;
+
                         int result = i * j;
                         string s = $"{result} / {i} = _____";
                         allMulti.Add(s);
