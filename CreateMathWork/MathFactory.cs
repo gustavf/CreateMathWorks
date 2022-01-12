829b1ff33d3a49ba8cb0103b62e61a11537ba328
gustav.frisholm <gustav.frisholm@acadiasoft.com>
2022-01-12 15:55:32 +0100

    fixing and tidy up

--- CreateMathWork/MathFactory.cs
+++ CreateMathWork/MathFactory.cs
@@ -2,83 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CreateMathWork
+namespace CreateMathWork;
+
+public class MathFactory
 {
-    public class MathFactory
+    public List<string> GetMathWork(bool isMulti, int max, string ignore = null)
     {
-        public List<string> GetMathWork(bool isMulti, string ignore = null)
+        var ignoreNumbers = ignore is null ? new List<string>() : ignore.Split(',').ToList();
+        var alldivision = isMulti ? CreateMulti(max, ignoreNumbers) : CreateDivision(max, ignoreNumbers);
+
+        Random random = new Random();
+        List<string> test = new List<string>();
+
+        for (int i = 0; i < 100; i++)
         {
-            //var alldivision = CreateAddition();
-            //var alldivision = CreateDivision();
-            var ignoreNumbers = ignore is null ? new List<string>() : ignore.Split(',').ToList();
-            var alldivision = isMulti ? CreateMulti(ignoreNumbers) : CreateDivision(ignoreNumbers);
+            int index = random.Next(alldivision.Count());
+            test.Add(alldivision[index]);
+        }
 
-            Random random = new Random();
-            List<string> test = new List<string>();
+        return test;
+    }
 
-            //for (int count = 0; count < numberOfTests; count++)
+
+    List<string> CreateMulti(int max, List<string> ignore)
+    {
+        List<string> allMulti = new List<string>();
+        for (int j = 1; j <= max; j++)
+        {
+            if (ignore.Any(x => x == j.ToString())) continue;
+
+            for (int i = 1; i <= max; i++)
             {
-                for (int i = 0; i < 100; i++)
-                {
-                    int index = random.Next(alldivision.Count());
-                    test.Add(alldivision[index]);
-                }
+                if (ignore.Any(x => x == i.ToString())) continue;
 
-                return test;
-            }
-
-            List<string> CreateAddition(List<string> ignore)
-            {
-                int max = 20;
-                List<string> allAddition = new List<string>();
-                for (int i = 1; i < max; i++)
-                {
-                    for (int j = 1; j < max; j++)
-                    {
-                        string s = $"{i} + {j} = _____";
-                        allAddition.Add(s);
-                    }
-                }
-                return allAddition;
-            }
-
-            List<string> CreateMulti(List<string> ignore)
-            {
-                List<string> allMulti = new List<string>();
-                for (int j = 1; j <= 10; j++)
-                {
-                    if (ignore.Any(x => x == j.ToString())) continue;
-
-                    for (int i = 1; i <= 10; i++)
-                    {
-                        if (ignore.Any(x => x == i.ToString())) continue;
-
-                        string s = $"{i} x {j} = _____";
-                        allMulti.Add(s);
-                    }
-                }
-
-                return allMulti;
-            }
-
-            List<string> CreateDivision(List<string> ignore)
-            {
-                List<string> allMulti = new List<string>();
-                for (int j = 1; j <= 10; j++)
-                {
-                    if (ignore.Any(x => x == j.ToString())) continue;
-                    for (int i = 1; i <= 10; i++)
-                    {
-                        if (ignore.Any(x => x == i.ToString())) continue;
-
-                        int result = i * j;
-                        string s = $"{result} / {i} = _____";
-                        allMulti.Add(s);
-                    }
-                }
-
-                return allMulti;
+                string s = $"{i} x {j} = _____";
+                allMulti.Add(s);
             }
         }
+
+        return allMulti;
+    }
+
+    List<string> CreateDivision(int max, List<string> ignore)
+    {
+        List<string> allMulti = new List<string>();
+        for (int j = 1; j <= max; j++)
+        {
+            if (ignore.Any(x => x == j.ToString())) continue;
+            for (int i = 1; i <= max; i++)
+            {
+                if (ignore.Any(x => x == i.ToString())) continue;
+                int result = i * j;
+
+                if (result > max) continue;
+
+                string s = $"{result} / {i} = _____";
+                allMulti.Add(s);
+            }
+        }
+
+        return allMulti;
     }
 }
